1c4d7e18cdd34acb74101ce1433708c2a9140588
Ryan Koch <koch.64@wright.edu>
2023-04-12 17:05:46 +0000

    bash script added

--- deployment/pullScript.sh
+++ deployment/pullScript.sh
@@ -1,4 +1,11 @@
-#! bin/bash
+#!/bin/sh
+# Pull latest version of the image
+docker pull koch64/first_nginx_image
+# Stop the container
+docker stop project5
+# Remove the container
+docker rm project5
 
-#this will be a bash script to pull from dockerhub
-
+docker image prune -f
+# Recreate the container
+docker run -p 80:80 -d --name project5 koch64/first_nginx_image
